ecffdad6ce0835692ba37cbfadfa1350224d6e67
EvGen Gal <ZBst.1@yandex.ru>
2024-10-05 05:02:56 +0300

    SRV. +env.const(перем.кмд.NODE_ENV), #main.ts(перем.,пуск)

---
+++ backend/src/common/envs/env.consts.ts
@@ -0,0 +1,5 @@
+// ^ перем. > кмд.пуска process.env.NODE_ENV
+const isProduction = process.env.NODE_ENV === 'production'
+const isDevelopment = process.env.NODE_ENV === 'development'
+
+export { isProduction, isDevelopment }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,41 @@
 import { NestFactory } from '@nestjs/core'
+import { config } from 'dotenv'
+
 import { AppModule } from './app.module'
+// константы > команды запуска process.env.NODE_ENV
+import { isProduction, isDevelopment } from './common/envs/env.consts.js'
+
+// загр.перем.окруж.от кмд.NODE_ENV
+if (isDevelopment) config({ path: '.env.development' })
+else if (isProduction) config()
 
 async function bootstrap() {
+	// созд.экзепл.прилож пока без передачи настр.
 	const app = await NestFactory.create(AppModule)
-	await app.listen(9753)
+
+	// PORT Запуска
+	let PORT: number
+	if (isDevelopment) PORT = +process.env.PORT
+	else if (isProduction) PORT = +process.env.SB_PG_PORT
+	else PORT = 3000
+
+	// прослуш.PORT и fn()callback с cg на Запуск
+	let url: string
+	await app.listen(PORT, () => {
+		// ^ вывод подкл.к БД от NODE_ENV. PROD (БД покаНЕТ) <> DEV (Local БД DTP)
+		let srt: string, port: string, source: string
+		if (isDevelopment) {
+			srt = 'DEV'
+			source = 'LocalHost'
+			port = `${process.env.LH_PG_PORT}(${source})`
+			url = process.env.PROTOCOL + process.env.PORT
+		} else if (isProduction) {
+			srt = 'PROD'
+			port = process.env.SB_PG_PORT + '(SupaBase)'
+			source = 'VERCEL'
+			url = process.env.VERCEL_URL
+		}
+		console.log(`${srt}. Сервер - ${port}, подключён '${source}' - ${url}`)
+	})
 }
 bootstrap()
